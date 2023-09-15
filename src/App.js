8af50c8a3caf9b0da00958f40547116b5b591926
UnchitthaP <unch.6672@gmail.com>
2023-09-15 15:47:08 +0700

    quiz-app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import Quiz from './component/Quiz';
+import Menu from './component/Menu';
+import Score from './component/Score';
+import {createContext,useState} from "react"
+import './App.css'
+
+export const DataContext = createContext()
+
+function App() {
+  const [appState,setAppState] = useState("menu")
+  const [score,setScore]=useState(0)
+  return (
+    <DataContext.Provider value={{appState,setAppState,score,setScore}}>
+    <div className="App">
+      <h1>Web Development Quiz</h1>
+      {appState === "menu" && <Menu/>}
+      {appState === "quiz" && <Quiz/>}
+      {appState === "score" && <Score/>} 
+    </div>
+    </DataContext.Provider>
+  );
+}
+
+export default App;
